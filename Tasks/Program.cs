b7cb48c68d1408bccbc87f627cde2a074de54c1d
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-04-12 09:31:44 +0500

    task 2

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -10,11 +10,11 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            List<String> str_list = new List<string>() {"saqib_single","mateen_legend","dr_khadim","ibrahim_programer"};
-            str_list.Sort();
-            for (int i = 0; i < str_list.Count; i++)
+            List<float> list = new List<float>() {3.0f,8.9f,2.5f,6.3f};
+            list.Sort();
+            for (int i = 0; i < list.Count; i++)
             {
-                Console.WriteLine(str_list[i]);
+                Console.WriteLine(list[i]);
             }
             Console.ReadKey();
         }

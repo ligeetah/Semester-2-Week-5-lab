b41b02b8a5896fcb208ca88b23f09c778844bd1b
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-04-12 09:29:00 +0500

    Add project files.

---
+++ Tasks/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> str_list = new List<string>() {"saqib_single","mateen_legend","dr_khadim","ibrahim_programer"};
+            str_list.Sort();
+            for (int i = 0; i < str_list.Count; i++)
+            {
+                Console.WriteLine(str_list[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}

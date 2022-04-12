2f568d44f48ad406dc49b404abcd2628b99b4827
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-04-12 09:41:10 +0500

    task 3

---
+++ Tasks/BL/test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.BL
+{
+    internal class test
+    {
+        public float member;
+        public test(float str)
+        {
+            member = str;
+        }
+    }
+}
--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Tasks.BL;
 namespace Tasks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            List<float> list = new List<float>() {3.0f,8.9f,2.5f,6.3f};
-            list.Sort();
+            test test = new test(6.5f);
+            test test1 = new test(4.3f);
+            test test2 = new test(2.9f);
+            List<test> list = new List<test>() {test,test1,test2};
+            list = list.OrderBy(o => o.member).ToList();
             for (int i = 0; i < list.Count; i++)
             {
-                Console.WriteLine(list[i]);
+                Console.WriteLine(list[i].member);
             }
             Console.ReadKey();
         }
